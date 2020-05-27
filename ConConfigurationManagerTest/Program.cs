f092fa1a3605c6591d74f583c9b94dea137e3e55
944414275 <944414275@qq.com>
2020-05-27 23:56:18 +0800

    ConfigurationManager

---
+++ ConConfigurationManagerTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConConfigurationManagerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _connectionString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            Console.WriteLine(_connectionString);
+            Console.ReadKey();
+        }
+    }
+}
