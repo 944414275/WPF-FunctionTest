550662fff78fcb975c2b8ab12ce7004ddd86f13a
944414275 <944414275@qq.com>
2025-01-24 10:06:44 +0800

    coolTower

---
+++ WpfCoolingTower/AirCompresser.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCoolingTower
+{
+    /// <summary>
+    /// AirCompresser.xaml 的交互逻辑
+    /// </summary>
+    public partial class AirCompresser : UserControl
+    {
+        public AirCompresser()
+        {
+            InitializeComponent(); 
+        }
+
+        public RunningState RunningState
+        {
+            get { return (RunningState)GetValue(RunningStateProperty); }
+            set { SetValue(RunningStateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RunningState.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RunningStateProperty =
+            DependencyProperty.Register("RunningState", typeof(RunningState), typeof(AirCompresser), new PropertyMetadata(default(RunningState), RunningStateChangedCallback));
+
+        private static void RunningStateChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RunningState value = (RunningState)e.NewValue;
+            VisualStateManager.GoToState(d as AirCompresser, value == RunningState.Error ? "errorState" : "normalSate", false);
+        }
+    }
+} 
